5fbb45a2e42bd5530e0346280620462326f0074e
sebastianmathes <s.mathes@posteo.de>
2020-08-18 22:27:35 +0200

    added error handling

--- snom_rc.py
+++ snom_rc.py
@@ -4,41 +4,41 @@ import sys
 from src import controls
 from src import usage
 
-if __name__ == "__main__":
-    phone_ip = input("Enter phone ip: ")
+if __name__ == '__main__':
+    phone_ip = input('Enter phone ip: ')
     while True:
-        command = input("Do something: ")
-        if command != "q":
+        command = input('Do something: ')
+        if command != 'q':
             # predefined values
-            if command == "ENTER":
-                controls.press_key(phone_ip,"ENTER")
-            elif command == "CANCEL":
-                controls.press_key(phone_ip,"CANCEL")
-            elif command[0:1] == "P":
+            if command == 'ENTER':
+                controls.press_key(phone_ip,'ENTER')
+            elif command == 'CANCEL':
+                controls.press_key(phone_ip,'CANCEL')
+            elif command[0:1] == 'P':
                 argument = command
                 controls.press_key(phone_ip,argument)
-            elif command[0:1] == "F":
+            elif command[0:1] == 'F':
                 argument = command
                 controls.press_key(phone_ip,argument)
 
 
             # arbitrary commands
-            elif command == "1":
-                argument = input("Enter key command: ")
+            elif command == '1':
+                argument = input('Enter key command: ')
                 controls.press_key(phone_ip,argument)
-            elif command == "2":
-                argument = input("Enter DTMF number: ")
+            elif command == '2':
+                argument = input('Enter DTMF number: ')
                 controls.press_key_dtmf(phone_ip,argument)
-            elif command == "3":
-                argument = input("Enter DTMF number: ")
+            elif command == '3':
+                argument = input('Enter DTMF number: ')
                 controls.press_number(phone_ip,argument)
 
             # help
-            elif command == "h" or command == "help" or command == "?":
+            elif command == 'h' or command == 'help' or command == '?':
                 usage.print_usage()
             else:
-                print("No valid input, use 'help'")
+                print('No valid input, use "help"')
 
         else:
-            print("Exiting...")
+            print('Exiting...')
             sys.exit(0)
--- src/controls.py
+++ src/controls.py
@@ -8,15 +8,27 @@ headers = {
 
 def press_key(phone_ip, key_value):
     url = f'http://{phone_ip}/command.htm?key={key_value}'
-    r = requests.get(url, headers=headers)
-    return r.status_code
+    try:
+        r = requests.get(url, headers=headers)
+        return r.status_code
+    except requests.exceptions.ConnectionError:
+        print("Connection error, check if IP and/or port are corret.")
+        return False
 
 def press_key_dtmf(phone_ip, key_dtmf_value):
     url = f'http://{phone_ip}/command.htm?key_dtmf={key_dtmf_value}'
-    r = requests.get(url, headers=headers)
-    return r.status_code
+    try:
+        r = requests.get(url, headers=headers)
+        return r.status_code
+    except requests.exceptions.ConnectionError:
+        print("Connection error, check if IP and/or port are corret.")
+        return False
 
 def press_number(phone_ip, number_value):
     url = f'http://{phone_ip}/command.htm?number={number_value}'
-    r = requests.get(url, headers=headers)
-    return r.status_code
+    try:
+        r = requests.get(url, headers=headers)
+        return r.status_code
+    except requests.exceptions.ConnectionError:
+        print("Connection error, check if IP and/or port are corret.")
+        return False
