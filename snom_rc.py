58bb9ce7c67a046f8674c2151830bf543f16e700
smathes <sebastian.mathes@nethinks.com>
2020-08-20 08:44:00 +0200

    codestyle

--- snom_rc.py
+++ snom_rc.py
@@ -11,29 +11,28 @@ if __name__ == '__main__':
         if command != 'q':
             # predefined values
             if command == 'ENTER':
-                controls.press_key(phone_ip,'ENTER')
+                controls.press_key(phone_ip, 'ENTER')
             elif command == 'CANCEL':
-                controls.press_key(phone_ip,'CANCEL')
+                controls.press_key(phone_ip, 'CANCEL')
             elif command[0:1] == 'P':
                 argument = command
-                controls.press_key(phone_ip,argument)
+                controls.press_key(phone_ip, argument)
             elif command[0:1] == 'F':
                 argument = command
-                controls.press_key(phone_ip,argument)
-
+                controls.press_key(phone_ip, argument)
 
             # arbitrary commands
             elif command == '1':
                 key_argument = controls.read_argument('key')
                 print(key_argument)
-                controls.press_key(phone_ip,key_argument)
+                controls.press_key(phone_ip, key_argument)
             elif command == '2':
                 dtmf_argument = controls.read_argument('number')
-                controls.press_key_dtmf(phone_ip,dtmf_argument)
+                controls.press_key_dtmf(phone_ip, dtmf_argument)
             elif command == '3':
                 number_argument = controls.read_argument('number')
                 print('Number is: ', number_argument)
-                controls.press_number(phone_ip,number_argument)
+                controls.press_number(phone_ip, number_argument)
 
             # help
             elif command == 'h' or command == 'help' or command == '?':
--- src/controls.py
+++ src/controls.py
@@ -7,34 +7,38 @@ headers = {
 }
 
 commands = ['OFFHOOK',
-        'ONHOOK',
-        'RIGHT',
-        'LEFT',
-        'UP',
-        'DOWN',
-        'VOLUME_UP',
-        'VOLUME_DOWN',
-        'MENU',
-        'REDIAL',
-        'DND',
-        'REC',
-        'SPEAKER',
-        'HEADSET',
-        'TRANSFER',
-        'F_HOLD',
-        '0','1','2','3','4','5','6','7','8','9']
+            'ONHOOK',
+            'RIGHT',
+            'LEFT',
+            'UP',
+            'DOWN',
+            'VOLUME_UP',
+            'VOLUME_DOWN',
+            'MENU',
+            'REDIAL',
+            'DND',
+            'REC',
+            'SPEAKER',
+            'HEADSET',
+            'TRANSFER',
+            'F_HOLD',
+            '0','1','2','3','4','5','6','7','8','9']
+
 
 def press_key(phone_ip, key_value):
     url = f'http://{phone_ip}/command.htm?key={key_value}'
     send_key(url)
 
+
 def press_key_dtmf(phone_ip, key_dtmf_value):
     url = f'http://{phone_ip}/command.htm?key_dtmf={key_dtmf_value}'
     send_key(url)
 
+
 def press_number(phone_ip, number_value):
     url = f'http://{phone_ip}/command.htm?number={number_value}'
     send_key(url)
+
 
 def send_key(url):
     try:
@@ -44,23 +48,25 @@ def send_key(url):
         print("Connection error, check if IP and/or port are corret.")
         return False
 
-def read_argument(mode):
-    argument = input("Which command? ")
-    if mode == "key":
-        if argument in commands:
-            return argument
-        elif argument == "list" or argument == "l":
-            list_commands()
-            read_argument(mode)
-        else:
-            print("Invalid command, try again or use l/list for help")
-            read_argument(mode)
 
-    elif mode == "number":
-        return argument
-    else:
-        print("Invalid mode, please check.")
-        return False
+def read_argument(mode):
+    while True:
+        argument = input("Which command? ")
+        if mode == "key":
+            if argument in commands:
+                return argument
+            elif argument == "list" or argument == "l":
+                list_commands()
+            else:
+                print("Invalid command, try again or use l/list for help")
+
+        elif mode == "number":
+            return argument
+
+        else:
+            print("Invalid mode, please check.")
+            return False
+
 
 def list_commands():
     for command in commands:
