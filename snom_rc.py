ba7ef9d3e126e5bfd800f1a6117aa05464150f80
sebastianmathes <s.mathes@posteo.de>
2020-08-22 14:30:51 +0200

    added login credentials for authentication

--- snom_rc.py
+++ snom_rc.py
@@ -7,33 +7,34 @@ from src import usage
 if __name__ == '__main__':
     usage.print_greeting()
     phone_ip = input('Enter phone ip: ')
+    wui_username = input('Enter snom WUI username: ')
+    wui_password = input('Enter snom WUI password: ')
+    login_credentials = [wui_username, wui_password]
     while True:
         command = input('Do something: ')
         if command != 'q':
             # predefined values
             if command == 'ENTER':
-                controls.press_key(phone_ip, 'ENTER')
+                controls.press_key(phone_ip, 'ENTER', login_credentials)
             elif command == 'CANCEL':
-                controls.press_key(phone_ip, 'CANCEL')
+                controls.press_key(phone_ip, 'CANCEL', login_credentials)
             elif command[0:1] == 'P':
                 argument = command
-                controls.press_key(phone_ip, argument)
+                controls.press_key(phone_ip, argument, login_credentials)
             elif command[0:1] == 'F':
                 argument = command
-                controls.press_key(phone_ip, argument)
+                controls.press_key(phone_ip, argument, login_credentials)
 
             # arbitrary commands
             elif command == '1':
                 key_argument = controls.read_argument('key')
-                print(key_argument)
-                controls.press_key(phone_ip, key_argument)
+                controls.press_key(phone_ip, key_argument, login_credentials)
             elif command == '2':
                 dtmf_argument = controls.read_argument('number')
-                controls.press_key_dtmf(phone_ip, dtmf_argument)
+                controls.press_key_dtmf(phone_ip, dtmf_argument, login_credentials)
             elif command == '3':
                 number_argument = controls.read_argument('number')
-                print('Number is: ', number_argument)
-                controls.press_number(phone_ip, number_argument)
+                controls.press_number(phone_ip, number_argument, login_credentials)
 
             # help
             elif command == 'h' or command == 'help' or command == '?':
--- src/controls.py
+++ src/controls.py
@@ -25,22 +25,22 @@ commands = ['OFFHOOK',
             '0','1','2','3','4','5','6','7','8','9']
 
 
-def press_key(phone_ip, key_value):
+def press_key(phone_ip, key_value, login_credentials):
     url = f'http://{phone_ip}/command.htm?key={key_value}'
-    send_key(url)
+    send_key(url, login_credentials)
 
 
-def press_key_dtmf(phone_ip, key_dtmf_value):
+def press_key_dtmf(phone_ip, key_dtmf_value, login_credentials):
     url = f'http://{phone_ip}/command.htm?key_dtmf={key_dtmf_value}'
-    send_key(url)
+    send_key(url, login_credentials)
 
 
-def press_number(phone_ip, number_value):
+def press_number(phone_ip, number_value, login_credentials):
     url = f'http://{phone_ip}/command.htm?number={number_value}'
-    send_key(url)
+    send_key(url, login_credentials)
 
 
-def send_key(url):
+def send_key(url, login_credentials):
     try:
         r = requests.get(url, headers=headers)
         return r.status_code
